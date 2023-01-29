c387ab50c467cd4a65a8e16b0b684c2565dd8987
jaysujae <jaysujae@gmail.com>
2023-01-29 19:48:06 +0800

    Reorganize project
    - use mean struct
    - use partial e_x and e_x_square to calculate for Q4

--- main.cpp
+++ main.cpp
@@ -16,22 +16,49 @@ public:
     }
 };
 
+struct mean{
+    uint64_t counts;
+    double value;
+
+    mean operator + (double num) {
+        auto new_counts = counts+1;
+        auto new_value = value * (counts) / (new_counts) + num / new_counts;
+        mean result = mean{counts+1, new_value};
+        return result;
+    }
+    mean operator + (mean other){
+        mean res;
+        res.counts = counts + other.counts;
+        res.value = value * ((double)counts / res.counts) + other.value * ((double)other.counts / res.counts);
+        return res;
+    }
+    mean operator - (mean other){
+        mean res;
+        res.counts = counts - other.counts;
+        res.value = value * (counts / res.counts) - other.value * (other.counts / res.counts);
+        return res;
+    }
+    mean& operator += (double num) {
+        counts += 1;
+        value *= (double)(counts - 1)/counts;
+        value += (double)num/counts;
+        return *this;
+    }
+};
+
 class Q1_Calculator : public Calculator<uint64_t>{
     private:
-        double e_x;
-        double e_x_square;
+        mean x;
+        mean x_square;
         uint64_t counts;
 
         void update_e_x(uint64_t num){
-            e_x *= (double)(counts-1)/counts;
-            e_x += (double)num/counts;
+            x += num;
+            return;
         }
         void update_e_x_square(uint64_t num){
-            e_x_square *= (double)(counts-1)/counts;
-            e_x_square += (double)(num*num)/counts;
-        }
-        double get_variance() {
-            return e_x_square - e_x * e_x;
+            x_square += num*num;
+            return;
         }
 
     public:
@@ -41,26 +68,34 @@ class Q1_Calculator : public Calculator<uint64_t>{
             update_e_x(num);
             update_e_x_square(num);
         }
+        double Get_Variance() {
+            return x_square.value - x.value * x.value;
+        }
         void Print()
         {
-            cout << "Q1 Variance : " << get_variance() << "\n";
+            cout << "Q1 Variance : " << Get_Variance() << "\n";
             return;
         }
+
+
+    Q1_Calculator operator + (Q1_Calculator &other) {
+         Q1_Calculator res;
+         res.counts = counts + other.counts;
+         res.x = x + other.x;
+         res.x_square = x_square + other.x_square;
+         return res;
+    }
 };
 
 class Q2_Calculator : public Calculator<double>{
     private:
         uint64_t counts;
-        double e_x;
+        mean x;
         double variance_times_counts;
-
-        void update_e_x(double num){
-            e_x *= (double)(counts-1)/counts;
-            e_x += num/counts;
-        }
-
-        double get_variance(){
-            return variance_times_counts/counts;
+        
+        void update_e_x(uint64_t num){
+            x += num;
+            return;
         }
 
     public:
@@ -68,25 +103,35 @@ class Q2_Calculator : public Calculator<double>{
     {   
         counts++;
 
-        auto old_e_x = e_x;
+        auto old_e_x = x.value;
 		update_e_x(num);
 
-		variance_times_counts += (num - e_x) * (num - old_e_x);
+
+		variance_times_counts += (num - x.value) * (num - old_e_x);
+    }
+
+    double Get_Variance(){
+        return variance_times_counts/counts;
     }
 
     void Print()
     {
-        cout << "Q2 Variance : " << get_variance() << "\n";
+        cout << "Q2 Variance : " << Get_Variance() << "\n";
         return;
     }
+
 };
 
 class Q3_Calculator : public Calculator<double>{
-    public:
+    private:
+        Q1_Calculator big_calculator;
+        Q1_Calculator small_calculator;
+        int border_line = 1000;
 
+    public:
     void Put(double num)
     {   
-        if(num > 1000){
+        if(num > border_line){
             big_calculator.Put(num);
             return;
         }
@@ -94,7 +139,7 @@ class Q3_Calculator : public Calculator<double>{
     }
     double Get_Variance()
     {
-        return big_calculator.Get_Variance() + small_calculator.Get_Variance();
+        return (big_calculator + small_calculator).Get_Variance();
     }
 
     void Print()
@@ -102,55 +147,68 @@ class Q3_Calculator : public Calculator<double>{
         cout << "Q3 Variance : " << Get_Variance() << "\n";
         return;
     }
-
-    private:
-    Q2_Calculator big_calculator;
-    Q2_Calculator small_calculator;
 };
 
 
 class Q4_Calculator : public Calculator<double>{
+    private:
+    uint64_t counts;
+    uint64_t current_timestamp;
+    vector<pair<uint64_t, mean>> e_x_window = vector<pair<uint64_t, mean>>();
+    vector<pair<uint64_t, mean>> e_x_square_window;
+    int capacity = 1000;
+
+    mean get_last_e_x(){
+        if(e_x_window.size() == 0){
+            return mean{0, 0};
+        }
+        return e_x_window.back().second;
+    }
+    mean get_last_e_x_square(){
+        if(e_x_square_window.size() == 0){
+            return mean{0, 0};
+        }
+        return e_x_square_window.back().second;
+    }
+
     public:
 
     void Put(double num)
     {   
         current_timestamp++;
         counts++;
+ 
+        e_x_window.push_back({current_timestamp, get_last_e_x() + num});
+        e_x_square_window.push_back({current_timestamp, get_last_e_x_square() + num*num});
 
-        num -= sample_mean;
-
-        sum_of_x += num;
-        sum_of_x_square += num * num;
-
-        prefix_sum_of_x[current_timestamp] = sum_of_x;
-        prefix_sum_of_x_square[current_timestamp] = sum_of_x_square;
+        if(e_x_window.size() > capacity){
+            e_x_window.erase(e_x_window.begin());
+            e_x_square_window.erase(e_x_square_window.begin());
+            return;
+        }
     }
-    double Get_Variance()
+
+    double Get_Variance(uint64_t starting_time)
     {
-        return sum_of_x_square / counts - pow(sum_of_x / counts, 2);
-    }
+        int diff = current_timestamp - starting_time;
+        auto loc = (int)e_x_window.size() - diff;
+        if(loc < 0){
+            loc = 0;
+        }
+        auto e_x = e_x_window[loc].second;
+        auto e_x_square = e_x_square_window[loc].second;
 
-    double Get_Variance(uint64_t starting_time){
-        auto sum_it = prefix_sum_of_x.lower_bound(starting_time);
-        auto square_sum_it = prefix_sum_of_x_square.lower_bound(starting_time);
+        auto partial_e_x = get_last_e_x() - e_x;
+        auto partial_e_x_square = get_last_e_x_square() - e_x_square;
 
-        return (sum_of_x_square - square_sum_it->second) / counts - pow( (sum_of_x - sum_it->second) / counts, 2);
+        return partial_e_x_square.value - partial_e_x.value * partial_e_x.value;
     }
 
     void Print()
     {
-        cout << " Q4 Variance : " << Get_Variance() << "\n";
+        cout << "Q4 Variance : " << Get_Variance(current_timestamp - 10) << "\n";
         return;
     }
-
-    private:
-    uint64_t counts;
-    double sample_mean;
-    uint64_t current_timestamp;
-    map<uint64_t, double> prefix_sum_of_x;
-    map<uint64_t, double> prefix_sum_of_x_square;
-    uint64_t sum_of_x;
-    uint64_t sum_of_x_square;
 };
 
 
